568641c98912bc519ed05c99f0b0898461f90518
Prawared Bowonphattharawadi <prawared.bw@gmail.com>
2019-08-26 16:04:04 +0700

    add generator

--- blocks/blocks_analog-temp.js
+++ blocks/blocks_analog-temp.js
@@ -1,15 +1,18 @@
-Blockly.Blocks['ky_013_analog_temp_block'] = {
-  init: function() {
-    this.appendDummyInput()
-        .appendField("KY-013 ANALOG TEMP");
-    this.appendValueInput("KY_013_ANALOG_TEMP_PIN")
-        .setCheck("Number")
-        .appendField("PIN");
-    this.setInputsInline(true);
-    this.setPreviousStatement(true, null);
-    this.setNextStatement(true, null);
-    this.setColour(230);
- this.setTooltip("");
- this.setHelpUrl("");
-  }
-};
+Blockly.Blocks["ky_013_analog_temp_block"] = {
+    init: function() {
+      this.appendDummyInput().appendField("KY-013 ANALOG TEMP");
+      this.appendDummyInput()
+        .appendField("PIN")
+        .appendField(new Blockly.FieldTextInput("A0"), "KY_013_ANALOG_TEMP_PIN")
+        .appendField("SET TO")
+        .appendField(
+          new Blockly.FieldVariable("KY_013_ANALOG_TEMP_VALUE"),
+          "KY_013_ANALOG_TEMP_VALUE"
+        );
+      this.setPreviousStatement(true, null);
+      this.setNextStatement(true, null);
+      this.setColour(230);
+      this.setTooltip("");
+      this.setHelpUrl("");
+    }
+  };
--- blocks/generators_analog-temp.js
+++ blocks/generators_analog-temp.js
@@ -1,6 +1,33 @@
-Blockly.JavaScript['ky_013_analog_temp_block'] = function(block) {
-  var value_ky_013_analog_temp_pin = Blockly.JavaScript.valueToCode(block, 'KY_013_ANALOG_TEMP_PIN', Blockly.JavaScript.ORDER_ATOMIC);
-  // TODO: Assemble JavaScript into code variable.
-  var code = '...;\n';
-  return code;
-};
+Blockly.JavaScript["ky_013_analog_temp_block"] = function(block) {
+    var text_ky_013_analog_temp_pin = block.getFieldValue(
+      "KY_013_ANALOG_TEMP_PIN"
+    );
+    var variable_ky_013_analog_temp_value = Blockly.JavaScript.variableDB_.getName(
+      block.getFieldValue("KY_013_ANALOG_TEMP_VALUE"),
+      Blockly.Variables.NAME_TYPE
+    );
+    // TODO: Assemble JavaScript into code variable.
+    var code = `
+    #VARIABLE
+	int KY_013_ANALOG_TEMP_PIN = ${text_ky_013_analog_temp_pin};
+	int Vo;
+	float R1 = 10000; // value of R1 on board
+	float logR2, R2, T;
+	float c1 = 0.001129148, c2 = 0.000234125, c3 = 0.0000000876741; //steinhart-hart coeficients for thermistor
+	#END
+
+	
+	  Vo = analogRead(KY_013_ANALOG_TEMP_PIN);
+	  R2 = R1 * (1023.0 / (float)Vo - 1.0); //calculate resistance on thermistor
+	  logR2 = log(R2);
+	  T = (1.0 / (c1 + c2*logR2 + c3*logR2*logR2*logR2)); // temperature in Kelvin
+	  T = T - 273.15; //convert Kelvin to Celcius
+	 // T = (T * 9.0)/ 5.0 + 32.0; //convert Celcius to Farenheit
+
+	  ${variable_ky_013_analog_temp_value} = T;
+
+	  delay(500);
+	
+    `;
+    return code;
+  };
